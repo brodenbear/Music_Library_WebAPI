44ae50d83ef8469fc76cab4fef4456db43db5152
Broden Berrett <brodenb@gmail.com>
2023-07-18 12:37:19 -0600

    init

---
+++ MusicLibraryWebAPI/Controllers/SongsController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicLibraryWebAPI.Data;
+using MusicLibraryWebAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicLibraryWebAPI.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SongsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<SongsController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var songs = _context.Songs.ToList();
+            return Ok(songs);
+        }
+
+        // GET api/<SongsController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var song = _context.Songs.Find(id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+            return Ok(song);
+        }
+
+        // POST api/<SongsController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Song song)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Songs.Add(song);
+                _context.SaveChanges();
+                return StatusCode(201, song);
+            }
+            return BadRequest(ModelState);
+        }
+
+        // PUT api/<SongsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SongsController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var song = _context.Songs.Find(id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+            _context.Songs.Remove(song);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
